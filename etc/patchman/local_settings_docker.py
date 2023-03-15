8acff69a059308279324d0155ce5dc322a722a0c
@ckbaker10 <->
2023-03-15 10:59:56 +0100

    Debugging Envs

--- etc/patchman/local_settings_docker.py
+++ etc/patchman/local_settings_docker.py
@@ -1,5 +1,10 @@
 
-CACHES = { }
+CACHES = {
+      'default': {
+          'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+          'LOCATION': '127.0.0.1:11211',
+      }
+  }
 DATABASES = { }
 
 if os.environ.get('PATCHMAN_DB_SQLLITE') is not None:
