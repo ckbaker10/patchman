fd701dfddfcf631ec36a38cc87686b953015c970
ckbaker10 <->
2023-04-13 16:49:54 +0200

    Docker-Compose fiddling

--- docker/startup.sh
+++ docker/startup.sh
@@ -8,14 +8,16 @@ python3 /build/manage.py migrate --run-syncdb
 set -x
 if [ -d ./certs ]; then
   if ls ./certs/*.crt 1> /dev/null 2>&1; then
+    rm -rf /usr/local/share/ca-certificates/*
+    update-ca-certificates
     cat ./certs/custom/*.crt >> /etc/ssl/certs/ca-certificates.crt
-    export SSL_CERT_DIR=/etc/ssl/certs
+    # openssl s_client -showcerts -servername packages.rundeck.com -connect packages.rundeck.com:443
     
     #if [ ! -f /usr/lib/python3.10/site-packages/certifi/cacert.pem.orig ]; then
     #  cp /usr/lib/python3.10/site-packages/certifi/cacert.pem /usr/lib/python3.10/site-packages/certifi/cacert.pem.orig
     #fi
     # Restore original content first if we are restarting here
-    cat /usr/lib/python3.10/site-packages/certifi/cacert.pem.orig > /usr/lib/python3.10/site-packages/certifi/cacert.pem
+    ## cat /usr/lib/python3.10/site-packages/certifi/cacert.pem.orig > /usr/lib/python3.10/site-packages/certifi/cacert.pem
     # Add custom certs to certifi truststore
     # https://stackoverflow.com/a/57910133
     ##cat ./certs/*.crt >> /usr/lib/python3.10/site-packages/certifi/cacert.pem
@@ -25,6 +27,9 @@ cert = /etc/ssl/certs/ca-certificates.crt
 EOF
   fi
 fi
+
+
+cp /etc/ssl/certs/ca-certificates.crt /usr/lib/python3.10/site-packages/certifi/cacert.pem
 
 UNQUOTED_BIND_ADDRESS=$(echo $BIND_ADDRESS | xargs)
 gunicorn patchman.wsgi -b $UNQUOTED_BIND_ADDRESS
